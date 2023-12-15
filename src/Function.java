cddc986022d1e831238e209e578da5f051249bf5
perkles <michal.albanodias@gamil.com>
2023-12-15 20:53:01 -0300

    subida

---
+++ src/Function.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Function {
+
+    public static String Title1(String str){
+        StringBuilder formatada = new StringBuilder();
+        str = str.toLowerCase();
+        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(str.split(" ")));
+        for (String frase: arrayList) {
+            formatada.append(frase.replace(frase.substring(0, 1), frase.substring(0, 1).toUpperCase()));
+            formatada.append(" ");
+        }
+        return formatada.toString();
+    }
+    public static String Title2(String str){
+        String formatada = "";
+        String[] array = str.split(" ");
+        for (int i = 0; i < array.length; i++) {
+            formatada += array[i].toUpperCase().substring(0,1)+array[i].substring(1);
+            formatada += " ";
+        }
+        return formatada;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = "hbo has announced the seventh season of game of thrones!";
+
+        System.out.println("function 1: " + Function.Title1(str));
+        System.out.println("function 2: " + Function.Title2(str));
+
+
+    }
+}
